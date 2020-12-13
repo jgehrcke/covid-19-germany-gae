12283550a00f17547d7273354bda78c72d34a32b
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2020-12-13 13:06:20 +0100

    auto-update: wip for csv-epsilon-merged rki csvs

--- tools/auto-update.sh
+++ tools/auto-update.sh
@@ -10,30 +10,48 @@ set -e
 
 BRANCH_NAME="data-update-$(date +"%m-%d")-${RNDSTR}"
 
-git checkout master
-git pull
-git branch "${BRANCH_NAME}" || true
-git checkout "${BRANCH_NAME}"
+# git checkout master
+# git pull
+# git branch "${BRANCH_NAME}" || true
+# git checkout "${BRANCH_NAME}"
 
 source tools/env.sh
 
-make update-csv
-git status --untracked=no --porcelain
-git commit data.csv -m "data.csv: update $(date +"%m-%d")" || true
+#make update-csv
+#git status --untracked=no --porcelain
+#git commit data.csv -m "data.csv: update $(date +"%m-%d")" || true
 
-make update-jhu-data
+#make update-jhu-data
 
+# Set previous data set aside. Use as "base" for merge later.
+set -x
+for RPATH in *-rki-*.csv; do
+    /bin/mv -f "${RPATH}" "${RPATH}.previous"
+done
+
+# Get current data set. Use as "extension" for merge later.
 python tools/build-rki-csvs.py
+
+# Set aside as "extension", then do a tolerant merge.
+for RPATH in *-rki-*.csv; do
+    /bin/mv -f "${RPATH}" "${RPATH}.current"
+    python tools/csv-epsilon-merge.py \
+    --threshold=6 --column-allowlist-pattern 'sum_*' \
+    "${RPATH}.previous" "${RPATH}.current" > \
+        "${RPATH}"
+done
+set +x
+
 git status --untracked=no --porcelain
-git commit -a -m "RKI data: update: $(date +"%m-%d")" || true
+#git commit -a -m "RKI data: update: $(date +"%m-%d")" || true
 
 python tools/build-rl-csvs.py
 git status --untracked=no --porcelain
-git commit -a -m "cases-rl-*: update: $(date +"%m-%d")" || true
+#git commit -a -m "cases-rl-*: update: $(date +"%m-%d")" || true
 
-python tools/plot-compare-sources.py
-git add gae/static/data-sources-comparison-2020-* gae/static/case-rate-rw-*
-git commit -a -m 'landing page update'
+#python tools/plot-compare-sources.py
+#git add gae/static/data-sources-comparison-2020-* gae/static/case-rate-rw-*
+#git commit -a -m 'landing page update'
 
 git push
 
